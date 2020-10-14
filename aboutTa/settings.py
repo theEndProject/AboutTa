fc79b2cfdeb9261f709ad8a4ffb2ea2075f44766
root <472437593@qq.com>
2020-10-14 14:13:35 +0800

    修改了csrf

--- aboutTa/settings.py
+++ aboutTa/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware', # 调试期间关闭
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
